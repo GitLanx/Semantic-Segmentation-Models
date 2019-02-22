601222a3aff01b57f077293685b02c06f4b06678
Lanxin-95 <675579465@qq.com>
2019-02-22 08:48:25 +0800

    add copy pretrained params from vgg

--- model_loader.py
+++ model_loader.py
@@ -2,6 +2,7 @@ from Models import FCN32
 from Models import FCN16
 from Models import FCN8
 from Models import UNet
+from Models.FCN32 import copy_params_from_vgg16
 
 MODELS = ['FCN32', 'UNet']
 
@@ -9,6 +10,7 @@ MODELS = ['FCN32', 'UNet']
 def load_model(model_name, input_shape, classes):
     if model_name == 'FCN32':
         model = FCN32.FCN32(classes, input_shape).build()
+        model = copy_params_from_vgg16(model)
     elif model_name == 'FCN16':
         model = FCN16.FCN16(classes, input_shape).build()
     elif model_name == 'FCN8':
